802bb99b826e739d1ecd838956591ad4c8f593b1
xiuye <xiuye_engineer@outlook.com>
2017-01-17 18:04:14 +0800

    verify test

--- src/com/xiuye/cert/util/CertUtil.java
+++ src/com/xiuye/cert/util/CertUtil.java
@@ -3,11 +3,52 @@ package com.xiuye.cert.util;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SignatureException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateFactory;
+import java.security.cert.CertificateNotYetValidException;
+import java.security.cert.X509Certificate;
+import java.util.Date;
 
 public class CertUtil {
+
+	public static void verifyValidity(String certPath ){
+		
+		X509Certificate cert = (X509Certificate) cert(certPath);
+		try {
+			cert.checkValidity(new Date());
+		} catch (CertificateExpiredException e) {
+			e.printStackTrace();
+		} catch (CertificateNotYetValidException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void verifySign(String fatherCertPath, String sonCertPath) {
+
+		Certificate son = cert(sonCertPath);
+		Certificate father = cert(fatherCertPath);
+		try {
+			son.verify(father.getPublicKey());
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	public static Certificate cert(String certPath) {
 		FileInputStream fis = null;
